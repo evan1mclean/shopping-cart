f98e052fb12840df52135e8f22fc89016dbc9372
Evan McLean <evanhughmclean@gmail.com>
2022-12-06 14:03:53 -0500

    Add header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,24 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
 function Header() {
   return (
-    <div className="Header">
-      
+    <div className= "text-white bg-sky-400 py-6">
+      <div className="container md:flex md:justify-between md:mx-auto">
+        <h1 className="text-4xl text-center md:mb-0 mb-4">
+          <a href="/">The Storefront</a>
+        </h1>
+        <div className="flex gap-5 text-lg justify-center items-center">
+          <div className="">
+            <a href="/">Home</a>
+          </div>
+          <div className="">
+            <a href="/products">Shop</a>
+          </div>
+          <button>
+            <AiOutlineShoppingCart />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
